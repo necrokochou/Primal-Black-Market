c9c1958b7a97c58f00d0c0207a5bb6ee4f9bc9bd
Joseph Aiden Reilly Flores <102833787+necrokochou@users.noreply.github.com>
2025-07-16 02:24:35 +0800

    Revert "Sync main to Database"

--- handlers/mongodbChecker.handler.php
+++ handlers/mongodbChecker.handler.php
@@ -2,13 +2,13 @@
 require_once BASE_PATH . '/bootstrap.php';
 require_once UTILS_PATH . '/envSetter.util.php';
 
-$mongoEnv = getMongoEnv();
-
 try {
-    $mongo = new MongoDB\Driver\Manager("mongodb://{$mongoEnv['host']}:{$mongoEnv['port']}");
+    $mongo = new MongoDB\Driver\Manager(
+      "mongodb://{$databases['mongoHost']}:{$databases['mongoPort']}"
+    );
 
     $mongo->executeCommand(
-        $mongoEnv['db'],
+        $databases['mongoDB'],
         new MongoDB\Driver\Command(['ping' => 1])
     );
 
