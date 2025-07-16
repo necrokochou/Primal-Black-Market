9fb2f6eda6d4933db8a6bdcee59d8460962b48d9
necrokochou <togami.playz2905@gmail.com>
2025-07-16 19:05:00 +0800

    refactor: refactor index.php to check database connections

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
 require_once BASE_PATH . '/bootstrap.php';
 require_once UTILS_PATH . '/envSetter.util.php';
 
-require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
-require_once HANDLERS_PATH . '/postgreChecker.handler.php';
+require HANDLERS_PATH . '/mongodbChecker.handler.php';
+require HANDLERS_PATH . '/postgresChecker.handler.php';
