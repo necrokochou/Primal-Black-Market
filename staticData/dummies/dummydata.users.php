2644e2b97e1c01089e339cd0b976c7ce74f18694
PhantomMorphus <louiejohnpamilar@gmail.com>
2025-07-16 20:29:39 +0800

    Refactor: Modified dummy datas and recompile

--- staticData/dummies/dummydata.users.php
+++ staticData/dummies/dummydata.users.php
@@ -3,50 +3,66 @@ return [
    [
         'Username' => 'phantommorphus',
         'Password' => 'phantom06240515543',
-        'Alias' => 'Pami',
+        'Alias' => 'ルイくん',
         'TrustLevel' => 10.0,
-        'IsVendor' => true
+        'IsVendor' => true,
+        'IsAdmin' => true
     ],
     [
         'Username' => 'Auditore',
         'Password' => 'requiescat',
+        'Email' => '202312060@fit.edu.ph',
         'Alias' => 'Aiden',
         'TrustLevel' => 6.1,
-        'IsVendor' => true
+        'IsVendor' => false,
+        "IsAdmin" => true
     ],
     [
         'Username' => 'necrokochou',
         'Password' => 'frieren',
         'Alias' => 'Otep',
         'TrustLevel' => 10.0,
-        'IsVendor' => true
+        'IsVendor' => true,
+        'IsAdmin' => true
     ],
     [
         'Username' => 'LlenG',
         'Password' => 'bakal',
         'Alias' => 'Allen',
         'TrustLevel' => 10.0,
-        'IsVendor' => true
+        'IsVendor' => true,
+        'IsAdmin' => true
     ],
     [
         'Username' => 'Cj',
         'Password' => 'thebest',
         'Alias' => 'Mother',
         'TrustLevel' => 10.0,
-        'IsVendor' => true
+        'IsVendor' => true,
+        'IsAdmin' => true
     ],
     [
         'Username' => 'Ikari',
         'Password' => 'ragebaiter',
         'Alias' => 'Ika',
         'TrustLevel' => 8.0,
-        'IsVendor' => false
+        'IsVendor' => false,
+        'IsAdmin' => false
     ],
     [
         'Username' => 'Boris',
         'Password' => 'femboy',
         'Alias' => 'Bor Bor',
         'TrustLevel' => 8.9,
-        'IsVendor' => false
+        'IsVendor' => false,
+        'IsAdmin' => false
     ],
+    [
+        'Username' => 'LingLingPing',
+        'Password' => 'tra_r6',
+        'Alias' => 'JM',
+        'TrustLevel' => 8.9,
+        'IsVendor' => false,
+        'IsAdmin' => false
+    ]
 ];
