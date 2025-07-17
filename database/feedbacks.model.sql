fbd5c375655b727b3c33d25569a4b5cc104b3401
PhantomMorphus <louiejohnpamilar@gmail.com>
2025-07-17 15:32:21 +0800

    Refactor: Fixed Tables

--- database/feedbacks.model.sql
+++ database/feedbacks.model.sql
@@ -5,6 +5,6 @@ CREATE TABLE feedback (
     "Rating" int CHECK ("Rating" BETWEEN 0 AND 5),
     "Comments" text NOT NULL,
     "PostedAt" date NOT NULL
-    FOREIGN KEY (ReviewerID) REFERENCES users(UserID),
-    FOREIGN KEY (VendorID) REFERENCES users(UserID)
+    FOREIGN KEY ("ReviewerID") REFERENCES users("UserID"),
+    FOREIGN KEY ("VendorID") REFERENCES users("UserID")
 );
--- database/listings.models.sql
+++ database/listings.models.sql
@@ -1,6 +1,7 @@
 CREATE TABLE listings (
     "ListingID" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     "VendorID" uuid NOT NULL,
+    "CategoriesID" uuid NOT NULL,
     "Title" varchar(256) NOT NULL,
     "Description" text NOT NULL,
     "Category" varchar(100) NOT NULL,
@@ -8,5 +9,6 @@ CREATE TABLE listings (
     "Quantity" int NOT NULL,
     "IsActive" boolean DEFAULT TRUE,
     "PublishDate" date NOT NULL,
-    FOREIGN KEY (VendorID) REFERENCES users(UserID)
+    FOREIGN KEY ("VendorID") REFERENCES users("UserID")
+    FOREIGN KEY ("CategoriesID") REFERENCES categories("CategoriesID")
 );
